f34d124de12ec22936649cc9728b84fa1004eaba
cheeraboina srilakshmi <kongalasrilakshmi@gmail.com>
2023-09-29 23:00:44 +0530

    hello
    small change

--- javascript/Function.js
+++ javascript/Function.js
@@ -1,68 +1,21 @@
-// function myFunction(text) {
-//   console.log(text);
-// }
-
-// myFunction("Hello World!");
-
-// // Function declaration
-// function add(a, b) {
-//   console.log(a + " " + b);
-// }
-
-// // Calling a function
-// add("sri", "nag");
-
-// // Function Expression
-// const nom = function (a1, b1) {
-//   console.log(a1 + b1);
-// };
-
-// // Calling function
-// nom(5, 8);
-
-// function Myname(g1, g2) {
-//   return g1 / g2;
-// }
-// const value = Myname(8, 2); // Calling the function
-// console.log(value);
-
-function calcAddition(number1, number2) {
-  return number1 - number2;
+function myFunction(text) {
+  console.log(text);
 }
-// //console.log(calcAddition(6, 9));
 
-let num = calcAddition("sri", "lakshmi");
-let num1 = calcAddition(6, 7);
+myFunction("Hello World!");
 
-console.log(num, num1);
-
-function calcAddition(number1, number2) {
-  return number1 - number2;
+// Function declaration
+function add(a, b) {
+  console.log(a + " " + b);
 }
-// //console.log(calcAddition(6, 9));
 
-let num = calcAddition("sri", "lakshmi");
-let num1 = calcAddition(6, 7);
+// Calling a function
+add("sri", "nag");
 
-console.log(num, num1);
+// Function Expression
+const nom = function (a1, b1) {
+  console.log(a1 + b1);
+};
 
-// function callAdd(x, y) {
-//   let z = x + y;
-//   return z;
-// }
-// console.log("Addition : " + callAdd(7, 4));
-
-// function Name() {
-//   let first = "sri",
-//     second = "anu",
-//     Third = "nag";
-//   return {
-//     first,
-//     second,
-//     Third,
-//   };
-// }
-// let { first, second, Third } = Name();
-// console.log(first);
-// console.log(second);
-// console.log(Third);
+// Calling function
+nom(5, 8);
