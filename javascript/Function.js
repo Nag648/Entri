fcb6f0b128eb5439320dd6233c711841617f0cdd
cheeraboina srilakshmi <kongalasrilakshmi@gmail.com>
2023-10-11 23:34:02 +0530

    hi

--- javascript/Function.js
+++ javascript/Function.js
@@ -17,5 +17,25 @@ const nom = function (a1, b1) {
   console.log(a1 + b1);
 };
 
+const name = "sri";
+const name1 = "nag";
+
+const fullName = [name, name1];
+console.log(fullName);
 // Calling function
 nom(5, 8);
+
+function Language() {
+  let first = "HTML",
+    second = "CSS",
+    Third = "Javascript";
+  return {
+    first,
+    second,
+    Third,
+  };
+}
+let { first, second, Third } = Language();
+console.log(first);
+console.log(second);
+console.log(Third);
